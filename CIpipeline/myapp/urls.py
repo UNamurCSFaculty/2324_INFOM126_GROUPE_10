32a779b7cb1fdcacfe74cd1736ed2b5c5d53c4a6
rosnyandersonkouanga <rosnyandersonkouanga@gmail.com>
2023-12-26 20:39:39 +0100

    ajout des fonctionnalites de suppression et modification des messages

--- CIpipeline/myapp/urls.py
+++ CIpipeline/myapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name='index'),
- path('ajouter_message/', views.add_message, name='add_message')
+path('ajouter_message/', views.add_message, name='add_message'),
+path('modifier_message/<int:message_id>/', views.update_message, name='modifier_message'),
+path('supprimer_message/<int:message_id>/', views.delete_message, name='supprimer_message')
     
 ]
--- CIpipeline/myapp/views.py
+++ CIpipeline/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 
 from .models import Message
 from .forms import MessageForm
@@ -15,3 +15,17 @@ def add_message(request):
     else:
         form = MessageForm()
     return render(request, 'add_message.html', {'form': form})
+def update_message(request, message_id):
+    message = get_object_or_404(Message, pk=message_id)
+    if request.method == 'POST':
+        form = MessageForm(request.POST, instance=message)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = MessageForm(instance=message)
+    return render(request, 'update.html', {'form': form, 'message': message})
+def delete_message(request, message_id):
+    message = get_object_or_404(Message, pk=message_id)
+    message.delete()
+    return redirect('index')
